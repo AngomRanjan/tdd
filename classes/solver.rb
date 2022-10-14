29f1ff76b222b0b299d43930a9700fb0ff2e9293
AngomRanjan <angomranjan@gmail.com>
2022-10-14 10:45:58 +0530

    Add Fizzbuzz Method

--- classes/solver.rb
+++ classes/solver.rb
@@ -6,9 +6,22 @@ class Solver
   def reverse(str)
     str.reverse
   end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      (num % 5).zero? ? 'buzz' : num
+    end
+  end
 end
 
-a = Solver.new
-puts a.factorial(3)
-
-puts a.reverse("hello")
+# a = Solver.new
+# puts a.factorial(3)
+# puts a.reverse('hello')
+# puts a.fizzbuzz(16)
+# puts a.fizzbuzz(18)
+# puts a.fizzbuzz(20)
+# puts a.fizzbuzz(30)
