b44838b57f86ef83ac06a05b464c75060c9f533e
AngomRanjan <angomranjan@gmail.com>
2022-10-14 10:34:28 +0530

    Add Reverse Method

--- classes/solver.rb
+++ classes/solver.rb
@@ -2,7 +2,13 @@ class Solver
   def factorial(num)
     num.zero? ? 1 : num * factorial(num - 1)
   end
+
+  def reverse(str)
+    str.reverse
+  end
 end
 
 a = Solver.new
 puts a.factorial(3)
+
+puts a.reverse("hello")
